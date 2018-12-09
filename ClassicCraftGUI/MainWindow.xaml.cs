e225d6fc302f33cc83e8f3971927c62af40fe9f3
Zwyk <erb.fel@gmail.com>
2018-12-09 14:14:44 +0100

    meh

--- ClassicCraftGUI/MainWindow.xaml.cs
+++ ClassicCraftGUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,10 +10,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace ClassicCraftGUI
 {
@@ -80,14 +83,30 @@ namespace ClassicCraftGUI
             e.Handled = REG2.IsMatch(e.Text);
         }
 
-        private void Debuff_Checked(object sender, RoutedEventArgs e)
+        private void Class_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
 
-        private void Class_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void PlayerTabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if(AddPlayer != null && AddPlayer.IsSelected)
+            {
+                string s = XamlWriter.Save(SampleTab);
 
+                StringReader stringReader = new StringReader(s);
+
+                XmlReader xmlReader = XmlTextReader.Create(stringReader, new XmlReaderSettings());
+                XmlReaderSettings sx = new XmlReaderSettings();
+
+                object x = XamlReader.Load(xmlReader);
+
+                PlayerTabs.Items.Insert(PlayerTabs.Items.Count-1, x);
+
+                ((TabItem)x).Header = "Player" + (PlayerTabs.Items.Count - 1);
+
+                PlayerTabs.SelectedIndex = PlayerTabs.Items.Count - 2;
+            }
         }
     }
 }
